4560da6dc7fd74c0ef1b2ae82de8af5aadc9486b
Emmanuel Kala <ekala@trinity.(none)>
2010-04-16 17:38:59 +0300

    Added event handlers for new UI components

--- src/main/java/org/creditsms/plugins/paymentview/ui/PaymentViewThinletTabController.java
+++ src/main/java/org/creditsms/plugins/paymentview/ui/PaymentViewThinletTabController.java
@@ -21,11 +21,15 @@ import net.frontlinesms.ui.i18n.InternationalisationUtils;
 import org.apache.log4j.Logger;
 import org.creditsms.plugins.paymentview.PaymentViewPluginController;
 import org.creditsms.plugins.paymentview.data.domain.Client;
+import org.creditsms.plugins.paymentview.data.domain.NetworkOperator;
+import org.creditsms.plugins.paymentview.data.domain.PaymentService;
 import org.creditsms.plugins.paymentview.data.domain.PaymentServiceTransaction;
 import org.creditsms.plugins.paymentview.data.repository.ClientDao;
 import org.creditsms.plugins.paymentview.data.repository.NetworkOperatorDao;
 import org.creditsms.plugins.paymentview.data.repository.PaymentServiceDao;
 import org.creditsms.plugins.paymentview.data.repository.PaymentServiceTransactionDao;
+
+import thinlet.Thinlet;
 
 /**
  * 
@@ -35,18 +39,39 @@ import org.creditsms.plugins.paymentview.data.repository.PaymentServiceTransacti
 public class PaymentViewThinletTabController implements ThinletUiEventHandler {
 //> UI FILES
 	private static final String UI_FILE_CLIENT_DIALOG = "/ui/plugins/paymentview/dgEditClient.xml";
+	private static final String UI_FILE_PAYMENT_SERVICE_DIALOG = "/ui/plugins/paymentview/dgEditPaymentService.xml";
+	private static final String UI_FILE_PAYMENT_SERVICE_TABLE = "/ui/plugins/paymentview/tbPaymentServices.xml";
+	private static final String UI_FILE_NETWORK_OPERATOR_TABLE = "/ui/plugins/paymentview/tbNetworkOperators.xml";
+	private static final String UI_FILE_NETWORK_OPERATOR_DIALOG = "/ui/plugins/paymentview/dgEditNetworkOperator.xml";
+	
 //> COMPONENT NAME CONSTANTS
 	private static final String COMPONENT_BT_NEW_CLIENT = "btNewClient";
 	private static final String COMPONENT_BT_DELETE_CLIENT = "btDeleteClient";
 	private static final String COMPONENT_BT_EDIT_CLIENT = "btEditClient";
+	private static final String COMPONENT_BT_ADD_NETWORK_OPERATOR = "btAddNetworkOperator";
+	private static final String COMPONENT_BT_REMOVE_NETWORK_OPERATOR = "btRemoveNetworkOperator";
 	private static final String COMPONENT_LS_CLIENTS = "lsClients";
+	private static final String COMPONENT_LS_ALL_NETWORK_OPERATORS = "lstAllOperators";
+	private static final String COMPONENT_LS_SELECTED_OPERATORS = "lstSelectedOperators";
 	private static final String COMPONENT_TBL_DISPERSALS = "tblDispersals";
 	private static final String COMPONENT_TBL_REPAYMENTS = "tblRepayments";
+	private static final String COMPONENT_TBL_NETWORK_OPERATORS = "tblNetworkOperators";
+	private static final String COMPONENT_TBL_PAYMENT_SERVICES = "tblPaymentServices";
 	private static final String COMPONENT_FLD_CLIENT_NAME = "fldClientName";
 	private static final String COMPONENT_FLD_PHONE_NUMBER = "fldPhoneNumber";
 	private static final String COMPONENT_FLD_OTHER_PHONE_NUMBER = "fldOtherPhoneNumber";
 	private static final String COMPONENT_FLD_EMAIL_ADDRESS = "fldEmailAddress";
+	private static final String COMPONENT_FLD_SERVICE_NAME = "fldServiceName";
+	private static final String COMPONENT_FLD_SMS_SHORT_CODE = "fldSmsShortCode";
+	private static final String COMPONENT_FLD_PIN_NUMBER = "fldPinNumber";
+	private static final String COMPONENT_FLD_SEND_MONEY_TEXT = "fldSendMoneyText";
+	private static final String COMPONENT_FLD_WITHDRAW_MONEY_TEXT = "fldWithdrawMoneyText";
+	private static final String COMPONENT_FLD_BALANCE_ENQUIRY_TEXT = "fldBalanceEnquiryText";
+	private static final String COMPONENT_FLD_OPERATOR_NAME = "fldOperatorName";
 	private static final String COMPONENT_DLG_CLIENT_DETAILS = "clientDetailsDialog";
+	private static final String COMPONENT_DLG_NETWORK_OPERATOR = "networkOperatorDialog";
+	private static final String COMPONENT_DLG_PAYMENT_SERVICE = "paymentServiceDialog";
+	private static final String COMPONENT_PN_SETTINGS_RIGHT_PANE = "pnSettingsRightPane";
 	
 //> CONSTANTS
 	private static final Logger LOG = Utils.getLogger(PaymentViewThinletTabController.class);
@@ -165,9 +190,9 @@ public class PaymentViewThinletTabController implements ThinletUiEventHandler {
 	}
 	
 	/**
-	 * Creates a {@link Thinlet} list items for the specified client
+	 * Returns a {@link Thinlet} list items for the specified client
 	 * @param client The client to represent as a node
-	 * @return node to insert to the thinlet list
+	 * @return list item to insert to the thinlet list
 	 */
 	private Object getListItem(Client client){	
 		Object node = uiController.createListItem(client.getName(), client);
@@ -175,7 +200,16 @@ public class PaymentViewThinletTabController implements ThinletUiEventHandler {
 	}
 	
 	/**
-	 * Creates a {@link Thinlet} UI table row containing the details of a {@link PaymentServiceTransaction}
+	 * Returns a {@link Thinlet} UI list item for the specified network operator
+	 * @param operator {@link NetworkOperator} instance represented by the list item
+	 * @return list item
+	 */
+	private Object getListItem(NetworkOperator operator){
+		return uiController.createListItem(operator.getOperatorName(), operator);
+	}
+	
+	/**
+	 * Returns a {@link Thinlet} UI table row containing the details of a {@link PaymentServiceTransaction}
 	 * @param transaction {@link PaymentServiceTransaction} to be displayed in the row
 	 * @return
 	 */
@@ -185,6 +219,33 @@ public class PaymentViewThinletTabController implements ThinletUiEventHandler {
 		uiController.add(row, uiController.createTableCell(transaction.getPaymentService().getServiceName()));
 		uiController.add(row, uiController.createTableCell(Double.toString(transaction.getAmount())));
 		uiController.add(row, uiController.createTableCell(InternationalisationUtils.getDatetimeFormat().format(transaction.getDate())));
+		return row;
+	}
+	
+	/**
+	 * Returns a {@link Thinlet} UI table row containing the name and short code of a {@link PaymentService}
+	 * @param service {@link PaymentService} to be displayed in the row
+	 * @return
+	 */
+	private Object getRow(PaymentService service){
+		Object row = uiController.createTableRow(service);
+		
+		uiController.add(row, uiController.createTableCell(service.getServiceName()));
+		uiController.add(row, uiController.createTableCell(service.getSmsShortCode()));
+		
+		return row;
+	}
+	
+	/**
+	 * Returns a {@link Thinlet} UI table row containing the name of a {@link NetworkOperator}
+	 * @param operator
+	 * @return
+	 */
+	private Object getRow(NetworkOperator operator){
+		Object row = uiController.createTableRow(operator);
+		
+		uiController.add(row, uiController.createTableCell(operator.getOperatorName()));
+		
 		return row;
 	}
 	
@@ -204,6 +265,24 @@ public class PaymentViewThinletTabController implements ThinletUiEventHandler {
 	 */
 	public PaymentServiceTransaction getPaymentServiceTransaction(Object component){
 		return (PaymentServiceTransaction)uiController.getAttachedObject(component);
+	}
+	
+	/**
+	 * Gets the {@link PaymentService} instance attached to the supplied component
+	 * @param component
+	 * @return the attached {@link PaymentService} instance
+	 */
+	public PaymentService getPaymentService(Object component){
+		return (PaymentService)uiController.getAttachedObject(component);
+	}
+	
+	/**
+	 * Gets the {@link NetworkOperator} instance attached to the supplied component
+	 * @param component
+	 * @return the attached {@link NetworkOperator} instance
+	 */
+	public NetworkOperator getNetworkOperator(Object component){
+		return (NetworkOperator)uiController.getAttachedObject(component);
 	}
 	
 	/**
@@ -334,7 +413,7 @@ public class PaymentViewThinletTabController implements ThinletUiEventHandler {
 		Object dialog = getClientDialog();
 		
 		// Check if a client object is attached to the client dialog
-		boolean clientExists = (getClient(dialog) == null);
+		boolean clientExists = (getClient(dialog) == null)?false:true;
 		
 		// Create/get client instance as appropriate
 		Client client = (clientExists == true)? getClient(dialog):new Client();
@@ -461,6 +540,239 @@ public class PaymentViewThinletTabController implements ThinletUiEventHandler {
 		}		
 		
 		return hashString;
+	}
+	
+	/**
+	 * Gets an instance of the payment services panel
+	 * @return
+	 */
+	private Object getPaymentServiceDialog(boolean load){
+		Object dialog = uiController.find(COMPONENT_DLG_PAYMENT_SERVICE);
+		
+		if(dialog == null)
+			dialog =  uiController.loadComponentFromFile(UI_FILE_PAYMENT_SERVICE_DIALOG, this);
+		
+		// Obtain reference to the "All Operators" list component
+		Object allOperators = uiController.find(dialog, COMPONENT_LS_ALL_NETWORK_OPERATORS);
+
+		// Populate the "All Operators" list
+		if(load){
+			for(NetworkOperator operator: networkOperatorDao.getAllNetworkOperators()){
+				Object item = getListItem(operator);
+				uiController.add(allOperators, item);
+			}
+		}
+		
+		return dialog;
+	}
+	
+	/**
+	 * Gets an instance of the payment services panel showing the details of the specified payment service
+	 * @param service {@link PaymentService} whose details are to be displayed on the panel
+	 * @return
+	 */
+	private Object getPaymentServiceDialog(PaymentService service){
+		Object dialog = getPaymentServiceDialog(true);
+		uiController.setAttachedObject(dialog, service);
+
+		uiController.setText(uiController.find(dialog, COMPONENT_FLD_SERVICE_NAME), service.getServiceName());
+		uiController.setText(uiController.find(dialog, COMPONENT_FLD_SMS_SHORT_CODE), service.getSmsShortCode());
+		uiController.setText(uiController.find(dialog, COMPONENT_FLD_PIN_NUMBER), service.getPinNumber());
+		uiController.setText(uiController.find(dialog, COMPONENT_FLD_SEND_MONEY_TEXT), service.getSendMoneyTextMessage());
+		uiController.setText(uiController.find(dialog, COMPONENT_FLD_WITHDRAW_MONEY_TEXT), service.getWithdrawMoneyTextMessage());
+		uiController.setText(uiController.find(dialog, COMPONENT_FLD_BALANCE_ENQUIRY_TEXT), service.getBalanceEnquiryTextMessage());
+		
+		Object selectedOperators = uiController.find(dialog, COMPONENT_LS_SELECTED_OPERATORS);		
+		
+		// Populate the network operators list for the selected payment service
+		for(NetworkOperator operator: service.getNetworkOperators()){
+			Object item = getListItem(operator);
+			uiController.add(selectedOperators, item);
+		}
+		
+		return dialog;
+	}
+	
+	
+	/**
+	 * Gets an instance of the network operator dialog
+	 * @return
+	 */
+	private Object getNetworkOperatorDialog(){
+		Object dialog = uiController.find(COMPONENT_DLG_NETWORK_OPERATOR);
+		
+		if(dialog == null)
+			dialog = uiController.loadComponentFromFile(UI_FILE_NETWORK_OPERATOR_DIALOG, this);
+		
+		return dialog;
+	}
+	
+	/**
+	 * Event handler for the settings tree menu
+	 * @param parentTab
+	 * @param treeView
+	 */
+	public void showSettingsItem(Object parentTab, Object treeView){
+		Object selectedNode = uiController.getSelectedItem(treeView);
+		uiController.removeAll(uiController.find(parentTab, COMPONENT_PN_SETTINGS_RIGHT_PANE));
+				
+		// No node has been selected
+		if(uiController.getName(selectedNode) == null)
+			return;
+		
+		// Change the contents of the right-most panel depending on the selected tree node 
+		if(uiController.getName(selectedNode).equals("ndPaymentSystems")){
+			Object table = getPaymentServicesTable();
+			uiController.add(uiController.find(parentTab, COMPONENT_PN_SETTINGS_RIGHT_PANE), uiController.getParent(table));
+			uiController.repaint();
+		}else if(uiController.getName(selectedNode).equals("ndNetworkOperators")){
+			Object table = getNetworkOperatorsTable(true);
+			uiController.add(uiController.find(parentTab, COMPONENT_PN_SETTINGS_RIGHT_PANE), uiController.getParent(table));
+			uiController.repaint();
+		}
+	}
+	
+	private Object getSettingsRightPane(){
+		return uiController.find(paymentViewTab, COMPONENT_PN_SETTINGS_RIGHT_PANE);
+	}
+	
+	/**
+	 * Returns a {@link Thinlet} UI table object containing the list of payment services in the system 
+	 * @return
+	 */
+	private Object getPaymentServicesTable(){		
+		Object table = uiController.find(getSettingsRightPane(), COMPONENT_TBL_PAYMENT_SERVICES); 
+		
+		if(table == null){
+			Object container = uiController.loadComponentFromFile(UI_FILE_PAYMENT_SERVICE_TABLE, this);
+			table = uiController.find(container, COMPONENT_TBL_PAYMENT_SERVICES);
+		}		
+		
+		for(PaymentService service: paymentServiceDao.getAllPaymentServices()){
+			Object row = getRow(service);
+			uiController.add(table, row);
+		}
+		return table;
+	}
+	
+	/**
+	 * Returns a {@link Thinlet} UI table object containing the list of network operators in the system
+	 * @param reload designates whether the table is to be reloaded with the list of operators
+	 * @return
+	 */
+	private Object getNetworkOperatorsTable(boolean reload){
+		Object table = uiController.find(getSettingsRightPane(), COMPONENT_TBL_NETWORK_OPERATORS);
+		
+		if(table == null){
+			Object container = uiController.loadComponentFromFile(UI_FILE_NETWORK_OPERATOR_TABLE, this);
+			table = uiController.find(container, COMPONENT_TBL_NETWORK_OPERATORS);
+		}
+		
+		if(reload){
+			for(NetworkOperator operator: networkOperatorDao.getAllNetworkOperators()){
+				Object row = getRow(operator);
+				uiController.add(table, row);
+			}
+		}
+		
+		return table;
+	}
+	
+	/**
+	 * Displays the payment service dialog
+	 */
+	public void showPaymentServiceDialog(){
+		uiController.add(getPaymentServiceDialog(true));
+	}
+	
+	/**
+	 * Displays the network operator dialog
+	 */
+	public void showNetworkOperatorDialog(){
+		uiController.add(getNetworkOperatorDialog());
+	}
+	
+	/**
+	 * Saves a payment service
+	 */
+	public void savePaymentService(){
+		
+	}
+	
+	/**
+	 * Saves a network operator record into the database
+	 */
+	public void saveNetworkOperator(){
+		// Fetch the dialog
+		Object dialog = getNetworkOperatorDialog();
+		
+		// Check if an existing record is being updated
+		boolean operatorExists = (getNetworkOperator(dialog) == null)?false:true;
+
+		// Fetch/Create a network operator instance as appropriate
+		NetworkOperator operator = (operatorExists == true)?getNetworkOperator(dialog) : new NetworkOperator();
+
+		// Fetch the name of the network operator
+		String name = uiController.getText(uiController.find(dialog, COMPONENT_FLD_OPERATOR_NAME));
+				
+		// Prevent saving of zero-length names
+		if(name.trim().length() == 0)
+			return;
+		
+		operator.setOperatorName(name);
+		
+		// Begin update/create
+		try{
+			if(operatorExists)
+				networkOperatorDao.updateNetworkOperator(operator);
+			else
+				networkOperatorDao.saveNetworkOperator(operator);
+		}catch(DuplicateKeyException e){
+			LOG.debug("A network operator with the specified name already exists" + e);
+		}
+		
+		// If update operation, remove the selected row from the table
+		if(operatorExists){
+			Object row = uiController.getSelectedItem(getNetworkOperatorsTable(false));
+			if(row != null){
+				uiController.remove(row);
+			}
+		}
+		
+		// Close the dialog
+		removeDialog(dialog);
+		
+		// Add new/updated record to the display list
+		uiController.add(getNetworkOperatorsTable(false), getRow(operator));
+		uiController.repaint();
+	}
+	
+	/**
+	 * Toggles the enabled property of the buttons that move network operators between the lists
+	 * of available and selected operators
+	 * @param list
+	 * @param button
+	 */
+	public void toggleMoveOperatorButton(Object list, Object button){
+		if(uiController.getSelectedIndex(list) != -1)
+			uiController.setEnabled(button, true);
+		else
+			uiController.setEnabled(button, false);
+	}
+	
+
+	/**
+	 * Moves a network operator from one list to another
+	 * @param sourceList {@link Thinlet} UI list from which the item is to be moved
+	 * @param targetList {@link Thinlet} UI list to which the item is being moved
+	 */
+	public void moveNetworkOperator(Object sourceList, Object targetList){
+		Object selectedItem = uiController.getSelectedItem(sourceList);
+		
+		if(selectedItem != null){
+			uiController.remove(selectedItem);
+			uiController.add(targetList, selectedItem);
+		}
 	}
 
 }
