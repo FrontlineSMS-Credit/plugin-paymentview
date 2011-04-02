cf290e156dc97b9f448689f11ae6568a2beff053
Ian Onesmus Mukewa <ian@credit.frontlinesms.com>
2011-04-02 23:17:10 +0300

    Better design for Analytics yo mtch steps...

--- src/main/java/org/creditsms/plugins/paymentview/ui/handler/analytics/AnalyticsTabHandler.java
+++ src/main/java/org/creditsms/plugins/paymentview/ui/handler/analytics/AnalyticsTabHandler.java
@@ -1,34 +1,37 @@
 package org.creditsms.plugins.paymentview.ui.handler.analytics;
 
+import net.frontlinesms.ui.ThinletUiEventHandler;
 import net.frontlinesms.ui.UiGeneratorController;
 import net.frontlinesms.ui.handler.BaseTabHandler;
 
-public class AnalyticsTabHandler extends BaseTabHandler{
-	private static final String XML_ANALYTICS_TAB = "/ui/plugins/paymentview/analytics/tabanalytics.xml"; 
-	private Object analyticsTab; 
+public class AnalyticsTabHandler implements ThinletUiEventHandler{
+	private static final String XML_ANALYTICS_TAB = "/ui/plugins/paymentview/analytics/tabanalytics.xml";
+	
+	private Object analyticsTab;
+	private CreateDashBoardTabHandler createDashBoardHandler;
+	private UiGeneratorController ui; 
 	
 	public AnalyticsTabHandler(UiGeneratorController ui) {
-		super(ui);		
+		this.ui = ui; 
 		init();
 	}
-
-	@Override
-	public void refresh() {		
+		
+	public void refresh() {
 	}
 
-	@Override
-	protected Object initialiseTab() {
+	
+	protected Object init() {
 		analyticsTab = ui.loadComponentFromFile(XML_ANALYTICS_TAB, this);
+		createDashBoardHandler = new CreateDashBoardTabHandler(ui, this.analyticsTab);
+		
+		ui.add(analyticsTab, createDashBoardHandler.getMainPanel());
+		return analyticsTab;
+	}
+
+	public Object getTab(){
 		return analyticsTab;
 	}
 
 	//> EVENTS...
-	public void customizeClientDB(){		
-	}
-	
-	public void addClient(){			
-	}
-	
-	public void importClient(){		
-	}	
+
 }
--- src/main/java/org/creditsms/plugins/paymentview/ui/handler/analytics/CreateDashBoardTabHandler.java
+++ src/main/java/org/creditsms/plugins/paymentview/ui/handler/analytics/CreateDashBoardTabHandler.java
@@ -1,5 +1,33 @@
 package org.creditsms.plugins.paymentview.ui.handler.analytics;
 
-public class CreateDashBoardTabHandler {
+import net.frontlinesms.ui.ThinletUiEventHandler;
+import net.frontlinesms.ui.UiGeneratorController;
 
+public class CreateDashBoardTabHandler implements ThinletUiEventHandler{
+	private static final String XML_MAIN_CREATE_DASHBOARD_PANEL = "pnl_MainCreateDashboard";
+	private static final String XML_STEP_CREATE_SETTINGS = "/ui/plugins/paymentview/analytics/createdashboard/stepcreatesettings.xml";
+	private static final String XML_STEP_SELECT_CLIENTS  = "/ui/plugins/paymentview/analytics/createdashboard/stepselectclients.xml";
+	
+	private Object mainPanel;
+	private UiGeneratorController ui;
+	private Object stepsCreateSettings; 
+	
+	public CreateDashBoardTabHandler(UiGeneratorController ui, Object createDashboardTab) {
+		this.ui = ui;
+		mainPanel = ui.find(createDashboardTab, XML_MAIN_CREATE_DASHBOARD_PANEL);
+		stepsCreateSettings = ui.loadComponentFromFile(XML_STEP_CREATE_SETTINGS, this);
+		System.out.println("mainPanel>> " + mainPanel);
+		System.out.println("mainPanel>> " + stepsCreateSettings); 
+		ui.add(mainPanel, stepsCreateSettings);
+	}	
+	
+	public Object getMainPanel (){
+		return this.mainPanel;
+	}
+	
+	public void refresh(){		
+	}
+	
+	public void deinit(){
+	}
 }
