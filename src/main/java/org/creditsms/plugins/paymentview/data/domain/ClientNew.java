846cce34df9e21422084ee2157da9b929ad434a0
Roy Owino <Roy@.(none)>
2011-04-06 12:21:23 +0300

    Fixed unit test for ClientDao.

--- src/main/java/org/creditsms/plugins/paymentview/data/domain/ClientNew.java
+++ src/main/java/org/creditsms/plugins/paymentview/data/domain/ClientNew.java
@@ -1,15 +1,10 @@
 package org.creditsms.plugins.paymentview.data.domain;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -42,16 +37,6 @@ public class ClientNew {
 			nullable=false,
 			unique=true)
 	private long phoneNumber;
-	
-	@OneToMany(mappedBy="clientNew",
-			cascade = CascadeType.ALL,
-			fetch = FetchType.LAZY)
-	private List<OtherClientDetails> otherClientDetailsList;
-
-	@OneToMany(mappedBy="clientNew",
-			cascade = CascadeType.ALL,
-			fetch = FetchType.LAZY)
-	private List<Account> account;
 
 	public long getClientId() {
 		return clientId;
@@ -84,22 +69,4 @@ public class ClientNew {
 	public void setPhoneNumber(long phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-
-	public List<OtherClientDetails> getOtherClientDetailsList() {
-		return otherClientDetailsList;
-	}
-
-	public void setOtherClientDetailsList(
-			List<OtherClientDetails> otherClientDetailsList) {
-		this.otherClientDetailsList = otherClientDetailsList;
-	}
-
-	public List<Account> getAccount() {
-		return account;
-	}
-
-	public void setAccount(List<Account> account) {
-		this.account = account;
-	}
-	
 }
