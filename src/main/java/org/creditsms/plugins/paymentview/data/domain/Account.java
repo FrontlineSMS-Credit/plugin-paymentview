a65b9700c66df2d08ff69a21abbfed852edf7491
Alex Anderson <alex@frontlinesms.com>
2011-04-09 17:06:21 +0300

    Updated package name; removed unnecessary constructors from Account.

--- src/main/java/org/creditsms/plugins/paymentview/data/domain/Account.java
+++ src/main/java/org/creditsms/plugins/paymentview/data/domain/Account.java
@@ -30,28 +30,10 @@ public class Account {
 	@JoinColumn(name = "clientId", nullable = true)
 	private Client client;
 
-	public Account(long id, long accountNumber) {
-		super();
-		this.id = id;
-		this.accountNumber = accountNumber;
-	}
+	public Account() {}
 
 	public Account(long accountNumber) {
-		super();
 		this.accountNumber = accountNumber;
-	}
-
-	public Account(long id, long accountNumber, Client client) {
-		super();
-		this.id = id;
-		this.accountNumber = accountNumber;
-		this.client = client;
-	}
-
-	public Account(long accountNumber, Client client) {
-		super();
-		this.accountNumber = accountNumber;
-		this.client = client;
 	}
 
 	public long getAccountId() {
