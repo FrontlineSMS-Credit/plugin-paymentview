ebf6efc9ed3408989c50c89b001b1464c940414e
Alex Anderson <alex@frontlinesms.com>
2011-04-12 18:37:40 +0300

    Added payment processor interfaces.

---
+++ src/main/java/net/frontlinesms/payment/IncomingPaymentProcessor.java
@@ -0,0 +1,5 @@
+package net.frontlinesms.payment;
+
+public interface IncomingPaymentProcessor {
+
+}
---
+++ src/main/java/net/frontlinesms/payment/PaymentService.java
@@ -0,0 +1,11 @@
+package net.frontlinesms.payment;
+
+import java.math.BigDecimal;
+
+import org.creditsms.plugins.paymentview.data.domain.Account;
+
+public interface PaymentService {
+	void setIncomingPaymentProcessor(IncomingPaymentProcessor incomingPaymentProcessor);
+	void makePayment(Account account, BigDecimal amount);
+	void checkBalance(Account account);
+}
