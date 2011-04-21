2e28fb766bdb6c27d43dfbaa40f58d7d6d03b3ce
Ian Onesmus Mukewa <ian@credit.frontlinesms.com>
2011-04-21 20:43:12 +0300

    changes during and after Ian|Alex|Roy Drill

--- src/main/java/org/creditsms/plugins/paymentview/utils/FieldMeta.java
+++
@@ -1,9 +0,0 @@
-package org.creditsms.plugins.paymentview.utils;
-
-/**
- * @author ian <ian@credit.frontlinesms.com>
- * a simple tool to be used with Customize field
- */
-public @interface FieldMeta {
-	public String icon() default "";
-}
