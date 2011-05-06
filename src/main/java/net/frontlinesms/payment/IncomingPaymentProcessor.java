402e82f5ddecb420c37de2115909e476054680a7
Ian Onesmus Mukewa <ian@credit.frontlinesms.com>
2011-05-06 10:13:18 +0300

    working on the IncomingPaymentEventListeners

--- src/main/java/net/frontlinesms/payment/IncomingPaymentProcessor.java
+++
@@ -1,7 +0,0 @@
-package net.frontlinesms.payment;
-
-import org.creditsms.plugins.paymentview.data.domain.IncomingPayment;
-
-public interface IncomingPaymentProcessor {
-	void process(IncomingPayment payment);
-}
