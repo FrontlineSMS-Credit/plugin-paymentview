83b789a34f99088d5b6c6de9cf932c65f7715dc4
Ian Onesmus Mukewa <ian@credit.frontlinesms.com>
2011-06-28 16:38:08 +0300

    change to viewClient Analytics

--- src/main/java/org/creditsms/plugins/paymentview/ui/handler/tabanalytics/innertabs/steps/viewdashboard/SelectClientsTableHandler.java
+++ src/main/java/org/creditsms/plugins/paymentview/ui/handler/tabanalytics/innertabs/steps/viewdashboard/SelectClientsTableHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import net.frontlinesms.ui.UiGeneratorController;
 
 import org.creditsms.plugins.paymentview.PaymentViewPluginController;
+import org.creditsms.plugins.paymentview.data.domain.Account;
 import org.creditsms.plugins.paymentview.data.domain.Client;
 import org.creditsms.plugins.paymentview.data.domain.ServiceItem;
 import org.creditsms.plugins.paymentview.data.domain.Target;
@@ -18,10 +19,12 @@ public class SelectClientsTableHandler extends BaseSelectClientTableHandler {
 	private static final String XML_CLIENTS_TABLE = "/ui/plugins/paymentview/analytics/viewdashboard/clientsTable.xml";
 	private final ServiceItem serviceItem;
 	private final TargetDao targetDao;
-	
-	public SelectClientsTableHandler(UiGeneratorController ui, PaymentViewPluginController pluginController, ServiceItem serviceItem) {
+
+	public SelectClientsTableHandler(UiGeneratorController ui,
+			PaymentViewPluginController pluginController,
+			ServiceItem serviceItem) {
 		super(ui, pluginController);
-		this.serviceItem = serviceItem;	
+		this.serviceItem = serviceItem;
 		this.targetDao = pluginController.getTargetDao();
 	}
 
@@ -33,30 +36,34 @@ public class SelectClientsTableHandler extends BaseSelectClientTableHandler {
 	@Override
 	protected String getClientsPanelFilePath() {
 		return XML_CLIENTS_TABLE;
-	}	
-	
+	}
+
 	@Override
 	protected List<Client> getClients(String filter, int startIndex, int limit) {
-		if (clients.isEmpty()){
+		if (clients.isEmpty()) {
 			return Collections.emptyList();
-		}else{
-			if (clientsTablePager.getMaxItemsPerPage() < clients.size()){
+		} else {
+			if (clientsTablePager.getMaxItemsPerPage() < clients.size()) {
 				return clients.subList(startIndex, limit);
 			} else {
 				return clients;
 			}
 		}
 	}
-	
+
 	@Override
 	protected List<String> getAccounts(Client client) {
 		List<String> accountNumbers = new ArrayList<String>();
-		List<Target> tgtLst = this.targetDao.getTargetsByServiceItemByClient(serviceItem.getId(), client.getId());
-		
-		for(int i=0; i<tgtLst.size(); i++){
-			for(int j=0; j<accountNumbers.size();j++){
-				if(tgtLst.get(i).getAccount().getAccountNumber().equals(accountNumbers.get(j))){
-					accountNumbers.add(tgtLst.get(i).getAccount().getAccountNumber());
+		List<Account> clientAccounts = this.accountDao.getAccountsByClientId(client.getId());
+		List<Target> tgtLst = this.targetDao.getTargetsByServiceItemByClient(
+				serviceItem.getId(), client.getId());
+
+		for (int i = 0; i < tgtLst.size(); i++) {
+			for (int j = 0; j < clientAccounts.size(); j++) {
+				if (tgtLst.get(i).getAccount().getAccountNumber()
+						.equals(clientAccounts.get(j).getAccountNumber())) {
+					accountNumbers.add(tgtLst.get(i).getAccount()
+							.getAccountNumber());
 					break;
 				}
 			}
