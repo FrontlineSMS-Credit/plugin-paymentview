4df0ba776d44ffe779a1ed8e6a947764393678e1
Onesmus Mukewa <ian@credit.frontlinesms.com>
2011-07-25 12:45:31 +0300

    changes to CREDIT-58

--- src/main/java/org/creditsms/plugins/paymentview/ui/handler/tabsettings/SettingsTabHandler.java
+++ src/main/java/org/creditsms/plugins/paymentview/ui/handler/tabsettings/SettingsTabHandler.java
@@ -49,11 +49,10 @@ public class SettingsTabHandler extends BaseTabHandler implements EventObserver{
 	
 	public Object getRow(MpesaPaymentService paymentService) {
 		Object row = ui.createTableRow(paymentService);
-		//Object portNumber = ui.createTableCell(paymentService.);
-	    Object paymentServiceName = ui.createTableCell(paymentService.toString());
-		//Object balance = ui.createTableCell(paymentService.getBalance().toString());
+		Object paymentServiceName = ui.createTableCell(paymentService.toString());
+		Object balance = ui.createTableCell(paymentService.getBalance().toString());
 		ui.add(row, paymentServiceName);
-		//ui.add(row, balance);
+		ui.add(row, balance);
 		return row;
 	}
 
