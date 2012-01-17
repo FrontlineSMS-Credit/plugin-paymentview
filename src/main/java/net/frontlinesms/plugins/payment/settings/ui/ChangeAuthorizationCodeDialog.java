009f1d76f24b86c479aba4ffc4999910a332c58e
Roy Owino <roy@credit.frontlinesms.com>
2012-01-17 12:30:28 +0300

    setting authorization code working

---
+++ src/main/java/net/frontlinesms/plugins/payment/settings/ui/ChangeAuthorizationCodeDialog.java
@@ -0,0 +1,45 @@
+package net.frontlinesms.plugins.payment.settings.ui;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
+import net.frontlinesms.ui.UiGeneratorController;
+
+import org.creditsms.plugins.paymentview.ui.handler.base.BaseDialog;
+import org.creditsms.plugins.paymentview.userhomepropeties.authorizationcode.AuthorizationChecker;
+import org.creditsms.plugins.paymentview.userhomepropeties.authorizationcode.AuthorizationProperties;
+
+public class ChangeAuthorizationCodeDialog extends BaseDialog{
+	private static final String DIALOG_UPDATE_AUTHCODE_XML = "/ui/plugins/payment/settings/dlgChangeAuthSettings.xml";
+
+	public ChangeAuthorizationCodeDialog(UiGeneratorController ui) {
+		super(ui);
+		init();
+	}
+
+	private void init() {
+	   this.dialogComponent = ui.loadComponentFromFile(DIALOG_UPDATE_AUTHCODE_XML, this);
+	}
+
+	public void updateAuthCode(String existing, String newcode, String verifynew) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+		//Check existing code
+		if (AuthorizationChecker.authenticate(existing)){
+			if (newcode.length()>0 || verifynew.length()>0){
+				if (newcode.equals(verifynew)){
+					AuthorizationProperties authProperties = AuthorizationProperties.getInstance();
+					authProperties.setAuthCode(newcode);
+					authProperties.saveToDisk();
+
+					ui.infoMessage("Authorization code has been successfully updated.");
+					ui.remove(dialogComponent);
+				} else {
+					ui.infoMessage("The new and verify authorization codes do not match.");
+				}	
+			} else {
+				ui.infoMessage("The authorisation code cannot be empty.");
+			}
+		} else {
+			ui.infoMessage("Wrong existing code. Please re-enter the existing code.");
+		}
+	}
+}
--- src/main/java/net/frontlinesms/plugins/payment/settings/ui/PaymentViewSettingsRootPanelHandler.java
+++ src/main/java/net/frontlinesms/plugins/payment/settings/ui/PaymentViewSettingsRootPanelHandler.java
@@ -2,19 +2,22 @@ package net.frontlinesms.plugins.payment.settings.ui;
 
 import java.util.List;
 
+import net.frontlinesms.plugins.PluginController;
+import net.frontlinesms.plugins.PluginSettingsController;
 import net.frontlinesms.settings.FrontlineValidationMessage;
+import net.frontlinesms.ui.ThinletUiEventHandler;
 import net.frontlinesms.ui.UiGeneratorController;
 import net.frontlinesms.ui.settings.UiSettingsSectionHandler;
 
 public class PaymentViewSettingsRootPanelHandler implements
-		UiSettingsSectionHandler {
+		UiSettingsSectionHandler, ThinletUiEventHandler {
+	private static final String LAYOUT_XML = "/ui/plugins/payment/settings/pnAuthCode.xml";
 	private final UiGeneratorController ui;
 	private final Object panel;
-	
+
 	public PaymentViewSettingsRootPanelHandler(UiGeneratorController ui) {
 		this.ui = ui;
-		panel = ui.createPanel("pnPvSettings");
-		add(ui.createLabel("Here is a labl"));
+		this.panel = ui.loadComponentFromFile(LAYOUT_XML, this);
 	}
 	
 	private void add(Object component) {
@@ -33,6 +36,11 @@ public class PaymentViewSettingsRootPanelHandler implements
 		return null;
 	}
 
+	public void setAuthCode() {
+//		ui.alert("Yeah!!");
+		new ChangeAuthorizationCodeDialog(ui).showDialog();
+	}
+	
 	public String getTitle() {
 		return "Title of the thing";
 	}
