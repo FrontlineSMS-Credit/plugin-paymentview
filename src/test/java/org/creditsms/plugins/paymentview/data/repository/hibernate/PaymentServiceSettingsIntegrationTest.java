1cd0c8935205e4223bae203e89010e33a2016f1e
Alex Anderson <alex@frontlinesms.com>
2012-08-06 16:47:25 +0300

    Created integration test demonstrating CREDIT-318.

--- src/test/java/org/creditsms/plugins/paymentview/data/repository/hibernate/PaymentServiceSettingsIntegrationTest.java
+++ src/test/java/org/creditsms/plugins/paymentview/data/repository/hibernate/PaymentServiceSettingsIntegrationTest.java
@@ -1,5 +1,8 @@
 package org.creditsms.plugins.paymentview.data.repository.hibernate;
 
+import java.math.BigDecimal;
+
+import org.creditsms.plugins.paymentview.data.domain.IncomingPayment;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import net.frontlinesms.data.domain.PersistableSettingValue;
@@ -10,6 +13,8 @@ import net.frontlinesms.plugins.payment.service.PaymentService;
 public class PaymentServiceSettingsIntegrationTest extends HibernateTestCase {
 	@Autowired
 	private HibernatePaymentServiceSettingsDao dao;
+	@Autowired
+	private HibernateIncomingPaymentDao incomingPaymentDao;
 	
 	public void testSettingsValuesShouldNotBeDuplicatedOnUpdate() throws Exception {
 		// given
@@ -30,7 +35,38 @@ public class PaymentServiceSettingsIntegrationTest extends HibernateTestCase {
 	}
 
 	private int countPersistedValues() {
-		return dao.getHibernateTemplate().loadAll(PersistableSettingValue.class).size();
+		return countPersistedValues(PersistableSettingValue.class);
+	}
+	
+	private int countPersistedValues(Class<?> entityClass) {
+		return dao.getHibernateTemplate().loadAll(entityClass).size();
+	}
+	
+	public void testDeletingServiceWithAssociatedIncomingPaymentsWillComplete() throws Exception {
+		//given
+		PersistableSettings s = new PersistableSettings(PaymentService.class, TestPaymentService.class);
+		s.set("key", 1);
+		assertEquals(0, countPersistedValues());
+		dao.saveServiceSettings(s);
+		assertEquals(1, countPersistedValues());
+		assertEquals(0, countPersistedValues(IncomingPayment.class));
+		incomingPaymentDao.saveIncomingPayment(createIncomingPayment(s));
+		assertEquals(1, countPersistedValues(IncomingPayment.class));
+		
+		// when
+		dao.deleteServiceSettings(s);
+		
+		// then
+		assertEquals(0, countPersistedValues());
+		assertEquals(1, countPersistedValues(IncomingPayment.class));
+		assertNull(incomingPaymentDao.getActiveIncomingPayments().get(0).getServiceSettings());
+	}
+
+	private IncomingPayment createIncomingPayment(PersistableSettings s) {
+		IncomingPayment ip = new IncomingPayment("bob", "+234567", new BigDecimal("2.50"), 0, null, null);
+		ip.setServiceSettings(s);
+		ip.setActive(true);
+		return ip;
 	}
 }
 
